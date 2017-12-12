294723fa8c93ed359c06f9e4626e51cb10e63f47
Vern Kofford <cheow3@gmail.com>
2017-12-12 15:44:54 -0700

    Updated doc order to descending

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,6 @@
      title: "Document #{d}",
      description: "Lorem impsum dolor sit amet",
      file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing",
-     image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg')
+     image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg'
+     )
 end
