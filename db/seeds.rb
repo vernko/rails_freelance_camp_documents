aa2ac6081d1e03dee18ef3534fdb7a0e8faada83
Vern Kofford <cheow3@gmail.com>
2018-04-27 05:17:45 -0600

    Updated rails-hmtl-sanitizer to 1.0.4 for security

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# create_table "freelance_documents", force: :cascade do |t|
-#     t.string "title"
-#     t.string "description"
-#     t.text "file_url"
-#     t.text "image_url"
-#     t.datetime "created_at", null: false
-#     t.datetime "updated_at", null: false
-#   end
-
 10.times do |d|
  FreelanceDocument.create!(
      title: "Document #{d}",
